650ce47deb9e821b060ebc93ea98067d39d24f71
Matthijs van Henten <matthijs@ischen.nl>
2014-07-25 15:11:20 +0200

    fixed the init again

--- bin/cli.js
+++ bin/cli.js
@@ -2,65 +2,35 @@
 
 'use strict';
 
-var Mycelium = require('../index'),
-    program = require('commander'),
-    Path = require('path');
+var init = require('../lib/init'),
+    Path = require('path'),
+    program = require('commander');
 
-var OPTIONS = [
-    {
-        name: 'template',
-        default: null,
-        option: ['-t, --template <template>', 'specify a global template file', String]
-  },
-    {
-        name: 'engine',
-        default: 'swig',
-        option: ['-e, --engine <name>', 'specify the templating engine', String]
-  },
-    {
-        name: 'port',
-        default: 3000,
-        option: ['-p, --port <port>', 'specify the port [3000]', Number]
-  },
-];
-
-OPTIONS.forEach(function(opt) {
-    program.option.apply(program, opt.option);
+program.on('--help', function() {
+    console.log('Usage:\n');
+    console.log('$ site.js --watch build\n');
+    console.log('$ site.js --port 3210 serve\n');
+    console.log('');
 });
 
-function help() {
-    console.log('Usage:\n');
-    console.log('$ cmd.js build content public\n');
-    console.log('$ cmd.js serve public\n');
-    console.log('');
-}
 
-function globals(src, dst) {
-    return {
-        source: Path.resolve(src),
-        target: Path.resolve(dst),
-        globals: OPTIONS.reduce(function(globals, value) {
-            globals[value.name] = program[value.name] || value.default;
-            return globals;
-        }, {})
-    };
-}
-
-program.on('--help', help);
+program.option('-t, --template <template>', 'specify a global template [index.swig]', String);
+program.option('-p, --template-path <path>', 'template path [./template]', String);
+program.option('-o, --output <name>', 'name of the output file [index.js]', String);
 
 program
-    .command('init <src> <dst>')
-    .description('init site')
-    .action(function(src, dst) {
-        Mycelium.init(globals(src, dst));
-    });
+    .command('init')
+    .description('initializing website')
+    .action(function(source, target) {
+        var config = {
+            source: source,
+            target: target,
+            output: program.output || 'index.js',
+            templatePath: Path.resolve(program.templatePath || './template'),
+            template: program.template || '/index.swig'
+        };
 
-program
-    .command('*')
-    .action(function() {
-        console.error('ERR: No action specified\n');
-        help();
+        init(config, process.exit);
     });
-
 
 program.parse(process.argv);
