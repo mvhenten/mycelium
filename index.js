7eba22e1dcb468167f9155b0225f595002b5b7c5
Matthijs van Henten <matthijs@ischen.nl>
2014-07-25 15:12:42 +0200

    move cli into toplevel index

--- index.js
+++ index.js
@@ -2,10 +2,35 @@
 
 'use strict';
 
-module.exports = {
-    init: require('./lib/init'),
-    build: require('./lib/build'),
-    serve: require('./lib/serve'),
-    cli: require('./lib/cli'),
-    run: require('./lib/run')
-};
+var init = require('./lib/init'),
+    Path = require('path'),
+    program = require('commander');
+
+program.on('--help', function() {
+    console.log('Usage:\n');
+    console.log('$ site.js --watch build\n');
+    console.log('$ site.js --port 3210 serve\n');
+    console.log('');
+});
+
+
+program.option('-t, --template <template>', 'specify a global template [index.swig]', String);
+program.option('-p, --template-path <path>', 'template path [./template]', String);
+program.option('-o, --output <name>', 'name of the output file [index.js]', String);
+
+program
+    .command('init')
+    .description('initializing website')
+    .action(function(source, target) {
+        var config = {
+            source: source,
+            target: target,
+            output: program.output || 'index.js',
+            templatePath: Path.resolve(program.templatePath || './template'),
+            template: program.template || '/index.swig'
+        };
+
+        init(config, process.exit);
+    });
+
+program.parse(process.argv);
